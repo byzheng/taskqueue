f47aadaa06fa920d8069264bf7f69ae4609462ef
Bangyou Zheng @ RStudio Server <bangyou.zheng@csiro.au>
2024-02-15 15:46:32 +1100

    Remove temp codes

--- tests/testthat/test-00-options.R
+++ tests/testthat/test-00-options.R
@@ -1,9 +1,4 @@
 test_that("test options", {
-    taskqueue_options(host = Sys.getenv("PGTESTHOST"),
-                      port = Sys.getenv("PGTESTPORT"),
-                      user = Sys.getenv("PGTESTUSER"),
-                      password = Sys.getenv("PGTESTPASSWORD"),
-                      database = Sys.getenv("PGTESTDATABASE"))
     skip_if(!is_test_db())
     expect_no_error({
         taskqueue_options()
